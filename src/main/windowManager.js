f64141c7238508be3cd9ea441e48e7b3944c3db8
lee jeunghyeun <jeunghyeun.lee@abcpharm.net>
2026-06-09 11:38:39 +0900

    feat(window): windowManager.js createOverlayWindow 함수 추가

--- src/main/windowManager.js
+++ src/main/windowManager.js
@@ -5,6 +5,35 @@ class WindowManager {
   constructor() {
     this.overlayWindow = null
   }
+
+  createOverlayWindow() {
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize
+
+    this.overlayWindow = new BrowserWindow({
+      width,
+      height,
+      x: 0,
+      y: 0,
+      frame: false,
+      transparent: true,
+      alwaysOnTop: true,
+      skipTaskbar: true,
+      resizable: false,
+      focusable: false,
+      webPreferences: {
+        preload: path.join(__dirname, '../preload/preload.js'),
+        contextIsolation: true,
+        nodeIntegration: false,
+      },
+    })
+
+    this.overlayWindow.setIgnoreMouseEvents(true, { forward: true })
+    this.overlayWindow.loadFile(
+      path.join(__dirname, '../renderer/overlay/index.html')
+    )
+
+    return this.overlayWindow
+  }
 }
 
 module.exports = WindowManager
