263c3ff85a9d22e1ab9fb98b19f2a9ed58e0c4ae
lee jeunghyeun <jeunghyeun.lee@abcpharm.net>
2026-06-09 11:38:06 +0900

    feat(window): windowManager.js 기본 구조 추가

---
+++ src/main/windowManager.js
@@ -0,0 +1,10 @@
+const { BrowserWindow, screen } = require('electron')
+const path = require('path')
+
+class WindowManager {
+  constructor() {
+    this.overlayWindow = null
+  }
+}
+
+module.exports = WindowManager
