54d7708034f132a72b909c06fecba166313204dd
lee jeunghyeun <jeunghyeun.lee@abcpharm.net>
2026-06-09 16:23:45 +0900

    feat(launcher): windowManager.js createTray 함수 추가

--- src/main/windowManager.js
+++ src/main/windowManager.js
@@ -63,6 +63,25 @@ class WindowManager {
     return this.launcherWindow
   }
 
+  createTray(onQuit) {
+    const iconPath = path.join(__dirname, '../assets/tray_icon.png')
+    const icon     = nativeImage.createFromPath(iconPath)
+
+    this.tray = new Tray(icon)
+    this.tray.setToolTip('Arcana')
+
+    const menu = Menu.buildFromTemplate([
+      { label: '런처 열기', click: () => this.createLauncherWindow() },
+      { type: 'separator' },
+      { label: '종료',     click: onQuit },
+    ])
+
+    this.tray.setContextMenu(menu)
+    this.tray.on('double-click', () => this.createLauncherWindow())
+
+    return this.tray
+  }
+
   toggleMouseEvents(ignore) {
     if (!this.overlayWindow) return
     this.overlayWindow.setIgnoreMouseEvents(ignore, { forward: true })
