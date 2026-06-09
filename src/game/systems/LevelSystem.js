c3cb8ccb200c002b4384cc370f826e5cbac73bd6
lee jeunghyeun <jeunghyeun.lee@abcpharm.net>
2026-06-09 15:14:07 +0900

    feat(level): LevelSystem.js addExperience 함수 추가

--- src/game/systems/LevelSystem.js
+++ src/game/systems/LevelSystem.js
@@ -7,6 +7,20 @@ class LevelSystem {
   getExpRequired(level) {
     return Math.floor(Math.pow(level, 1.5) * 100)
   }
+
+  addExperience(pet, amount) {
+    let { level, exp } = pet
+    exp += amount
+
+    while (exp >= this.getExpRequired(level)) {
+      exp -= this.getExpRequired(level)
+      level = this.levelUp(pet, level)
+    }
+
+    this.Pet.updatePet(pet.id, { level, exp })
+    this.save()
+    return { level, exp }
+  }
 }
 
 module.exports = LevelSystem
