411a8747a1a02d6f7dd8496d3c0384165f5419a3
lee jeunghyeun <jeunghyeun.lee@abcpharm.net>
2026-06-09 13:09:38 +0900

    feat(preload): preload.js pet API 노출 추가

--- src/preload/preload.js
+++ src/preload/preload.js
@@ -1,3 +1,10 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
-contextBridge.exposeInMainWorld('arcana', {})
+contextBridge.exposeInMainWorld('arcana', {
+  pet: {
+    getAll: () => ipcRenderer.invoke('pet:get-all'),
+  },
+  overlay: {
+    toggleMouse: (ignore) => ipcRenderer.send('overlay:toggle-mouse', ignore),
+  },
+})
