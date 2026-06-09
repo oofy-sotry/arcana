b16e2cbaddf73b37ab75005e7c2a35f3965724a6
lee jeunghyeun <jeunghyeun.lee@abcpharm.net>
2026-06-09 13:10:00 +0900

    feat(window): index.js GameWorld 연결

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,9 @@
 const { app } = require('electron')
+const GameWorld = require('./gameWorld')
+
+const gameWorld = new GameWorld()
 
 app.whenReady().then(async () => {
-
+  await gameWorld.init()
+  gameWorld.startTick()
 })
