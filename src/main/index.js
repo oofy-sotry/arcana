f604af13d4e49bdc9db9236cf9f1b8aed630e9af
lee jeunghyeun <jeunghyeun.lee@abcpharm.net>
2026-06-09 13:10:10 +0900

    feat(window): index.js WindowManager 연결

--- src/main/index.js
+++ src/main/index.js
@@ -1,9 +1,13 @@
 const { app } = require('electron')
-const GameWorld = require('./gameWorld')
+const GameWorld     = require('./gameWorld')
+const WindowManager = require('./windowManager')
 
-const gameWorld = new GameWorld()
+const gameWorld     = new GameWorld()
+const windowManager = new WindowManager()
 
 app.whenReady().then(async () => {
   await gameWorld.init()
   gameWorld.startTick()
+
+  windowManager.createOverlayWindow()
 })
